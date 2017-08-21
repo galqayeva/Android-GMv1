f985e998cb174e686fcc5d9a01dc5c19516a00a1
telim2 <telim2@pro.local>
2017-08-21 12:58:17 +0400

    model created

---
+++ app/src/main/java/com/example/telim2/gmv1/Adapters/Model.java
@@ -0,0 +1,39 @@
+package com.example.telim2.gmv1.Adapters;
+
+/**
+ * Created by telim2 on 21.08.2017.
+ */
+
+public class Model {
+    String lon,lan,name;
+
+    public Model(String lon, String lan, String name) {
+        this.lon = lon;
+        this.lan = lan;
+        this.name = name;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public String getLan() {
+        return lan;
+    }
+
+    public void setLan(String lan) {
+        this.lan = lan;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
