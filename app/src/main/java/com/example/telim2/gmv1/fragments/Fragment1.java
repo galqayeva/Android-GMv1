75129731d966486c3627dcbe1652e28aa13251a3
galqayeva <galqayeva@gmail.com>
2017-08-25 10:20:17 +0400

    gmc

--- app/src/main/java/com/example/telim2/gmv1/fragments/Fragment1.java
+++ app/src/main/java/com/example/telim2/gmv1/fragments/Fragment1.java
@@ -15,24 +15,14 @@ import com.example.telim2.gmv1.R;
  */
 
 public class Fragment1 extends Fragment {
-
-    private Button btn;
-    private TextView tw;
+    
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment1,container,false);
 
-        btn=(Button)view.findViewById(R.id.button);
-        tw=(TextView)view.findViewById(R.id.textView);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               tw.setText("okkay");
-            }
-        });
 
         return view;
 
