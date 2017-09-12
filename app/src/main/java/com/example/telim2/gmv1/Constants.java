3ba95466c4315b3fb58f2a76d446741ef2658036
galqayeva <galqayeva@gmail.com>
2017-09-12 16:23:42 +0400

    dd

---
+++ app/src/main/java/com/example/telim2/gmv1/Constants.java
@@ -0,0 +1,11 @@
+package com.example.telim2.gmv1;
+
+/**
+ * Created by telim2 on 12.09.2017.
+ */
+
+
+public class Constants {
+
+    String url2="http://172.16.200.200/GMv1/check.php?registerId=";
+}
