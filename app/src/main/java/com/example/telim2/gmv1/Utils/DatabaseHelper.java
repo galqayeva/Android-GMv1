6ec3582126d2ed366c08e58fa92ef40adbdaf093
galqayeva <galqayeva@gmail.com>
2017-10-12 17:47:02 +0400

    a

--- app/src/main/java/com/example/telim2/gmv1/Utils/DatabaseHelper.java
+++ app/src/main/java/com/example/telim2/gmv1/Utils/DatabaseHelper.java
@@ -1,8 +1,72 @@
 package com.example.telim2.gmv1.Utils;
 
-/**
- * Created by telim2 on 11.10.2017.
- */
 
-public class DatabaseHelper {
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "BMM.db";
+    public static final String TABLE_NAME = "bmm";
+    public static final String COL1 = "ID";
+    public static final String COL2 = "GRADE";
+    public static final String COL3 = "SUBJECT";
+    public static final String COL4 = "DAY";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                " GRADE TEXT,SUBJECT TEXT, DAY TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addData(String grade,String subject,String day) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, grade);
+        contentValues.put(COL3,subject);
+        contentValues.put(COL4,day);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public Cursor getListContents(String day){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("SELECT * FROM " + TABLE_NAME +" WHERE DAY="+day, null);
+        return data;
+    }
+
+    public Cursor getAlldata(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("SELECT * FROM " + TABLE_NAME , null);
+        return data;
+    }
+
+
+    public void deleteAll(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "DELETE FROM " + TABLE_NAME ;
+        db.execSQL(query);
+    }
 }
